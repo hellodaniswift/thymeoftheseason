d6a2dc0d185aa9f716b1a19bbf0241d1164fe519
danipatelle <109215494+danipatelle@users.noreply.github.com>
2023-03-14 13:15:52 +0000

    added servings adjustment function

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 document.querySelector('form').addEventListener('submit', function(event) {
   alert('you have successfully subscribed to our mailing list:-)!');
 });
+
+
+function adjustIngredients() {
+  var servings = document.getElementById("servings").value;
+  
+  var baseTomatoes = 8;
+  var baseGarlic = 2;
+  var baseOliveOil = 200;
+  var baseAnchovies = 6;
+  var basePasta = 200;
+  
+  var adjustedTomatoes = (baseTomatoes * servings / 4);
+  var adjustedGarlic = (baseGarlic * servings / 4);
+  var adjustedOliveOil = (baseOliveOil * servings / 4);
+  var adjustedAnchovies = (baseAnchovies * servings / 4);
+  var adjustedPasta = (basePasta * servings / 4);
+  
+  document.getElementById("tomatoes").innerHTML = adjustedTomatoes;
+  document.getElementById("garlic").innerHTML = adjustedGarlic;
+  document.getElementById("oliveOil").innerHTML = adjustedOliveOil;
+  document.getElementById("anchovies").innerHTML = adjustedAnchovies;
+  document.getElementById("pasta").innerHTML = adjustedPasta;
+}
+
+window.addEventListener('load', adjustIngredients);
